fd678f83094fae3054529003ef40b7314cc0d19b
trozeman <trozrman@gmail.com>
2017-05-01 22:29:03 +0300

    Start dev html 5 banner

--- js/gsap.js
+++ js/gsap.js
@@ -12,8 +12,7 @@ $(window).scroll(function() {
 	var top = $(window).scrollTop();
 	var proj = projects.offset().top;
 	var logs = logo.offset().top;
-		// console.log(top );
-	// console.log(proj);
+
 	if (top >= logs-300 && c == 1) {
 		c++;
 		tm.staggerTo(logo, 0.3, {rotationY:0, rotationX:0, opacity: 1,  scale: 1 ,ease: Power1.easeOut }, 0.1);
@@ -23,3 +22,18 @@ $(window).scroll(function() {
 		tm.staggerTo(projects, 0.5, {rotationY:0, rotationX:0, opacity: 1,  scale: 1 ,ease: Power1.easeOut }, 0.2);
 	}
 });
+var banner = $('.banner');
+var bannerText = $('.banner>.row>h3:nth-child(1)');
+var bannerText2 = $('.banner>.row>h3:nth-child(2)');
+var bannerText3 = $('.banner>.row>h3:nth-child(3)');
+var bh = banner.height();
+var bw = banner.width();
+var th = bannerText.height();
+var tw = bannerText.width();
+
+tm.to(bannerText, 1, {y:bh/2 - th/2, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeOut });
+tm.to(bannerText, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeOut }, '+=3');
+tm.to(bannerText2, 1, {y:bh/2 - th/2, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeOut }, 'bannerText-=1');
+tm.to(bannerText2, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeOut }, '+=3');
+tm.to(bannerText3, 1, {y:bh/2 - th/2, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeOut }, 'bannerText2-=1');
+tm.to(bannerText3, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeOut }, '+=3');
