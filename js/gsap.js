b2107e01687b241fdb4581164aed2a128ffb1317
Trozeman <trozeman@gmail.com>
2017-05-08 23:10:06 +0300

    Fix CSS and GSAP animation

--- js/gsap.js
+++ js/gsap.js
@@ -1,11 +1,11 @@
-var photo = $('.portret'); 
+var portret = $('.portret'); 
 var logo = $('.photo'); 
 var projects = $('.progect'); 
 var tm = new TimelineMax({}); 
 var ta = new TimelineMax({}); 
 var tb = new TimelineMax({}); 
 
-tm.from(photo, 1, {rotationY:90, rotationX:90, opacity: 0,  scale: 0.3, ease:Power1.easeOut }, 0.3);
+tm.from(portret, 1, {rotationY:90, rotationX:90, opacity: 0,  scale: 0.3, ease:Power1.easeOut }, 0.3);
 
 
 var m =1;
@@ -29,14 +29,15 @@ var banner = $('.banner');
 var bannerText = $('.banner>.row>h3:nth-child(1)');
 var bannerText2 = $('.banner>.row>h3:nth-child(2)');
 var bannerText3 = $('.banner>.row>h3:nth-child(3)');
+function play(){
 var bh = banner.height();
 var bw = banner.width();
 var th = bannerText.height();
 var tw = bannerText.width();
-
-tb.to(bannerText, 1, {y:bh/2 - th*1.5, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeInOut });
-tb.to(bannerText, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeInOut }, '+=3');
-tb.to(bannerText2, 1, {y:bh/2 - th*1.5, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeInOut }, 'bannerText-=1');
-tb.to(bannerText2, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeInOut }, '+=3');
-tb.to(bannerText3, 1, {y:bh/2 - th*1.5, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeInOut }, 'bannerText2-=1');
-tb.to(bannerText3, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeInOut }, '+=3');
+	tb.to(bannerText, 1, {y:bh/2 - th*1.5, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeInOut });
+	tb.to(bannerText, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeInOut }, '+=3');
+	tb.to(bannerText2, 1, {y:bh/2 - th*1.5, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeInOut }, 'bannerText-=1');
+	tb.to(bannerText2, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeInOut }, '+=3');
+	tb.to(bannerText3, 1, {y:bh/2 - th*1.5, rotationY:0, rotationX:0, opacity: 1,  scale: 1, ease:Power1.easeInOut }, 'bannerText2-=1');
+	tb.to(bannerText3, 1, {y:bh, rotationY:0, rotationX:90, opacity: 0,  scale: 0.8, ease:Power1.easeInOut }, '+=3');
+}
