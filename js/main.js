b78cb8a62b9750f9614e1513759cea351556295f
Trozeman <trozeman@gmail.com>
2019-02-10 01:11:58 +0200

    Mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let activeScreen;
 
 document.addEventListener("DOMContentLoaded", function () {
+    let navBar = document.getElementById("navBar");
+    let navBtn = document.getElementById("nav-btn");
     activeScreen = document.getElementById("about");
     if(window.location.hash){
         setTimeout(function () {
@@ -13,9 +15,19 @@ document.addEventListener("DOMContentLoaded", function () {
         i.onclick = function () {
             setTimeout(function () {
                 activeScreen = document.getElementById(window.location.hash.substr(1));
+                navBtn.click();
             }, 50)
         }
     });
+    navBtn.onclick = function () {
+        if(!navBar.classList.contains("open-nav")){
+            navBar.classList.add("open-nav");
+            this.classList.add("nav-btn-react");
+        }else {
+            navBar.classList.remove("open-nav");
+            this.classList.remove("nav-btn-react");
+        }
+    };
 });
 
 document.onmousewheel = function( e ) {
